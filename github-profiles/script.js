9dbd601e204c8d7b421fbdc12f93c25d195be292
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-26 15:13:09 +0100

    retouching

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,3 +1,5 @@
+
+
 const APIURL = 'https://api.github.com/users/'
 
 const main = document.getElementById('main')
@@ -7,10 +9,10 @@ const search = document.getElementById('search')
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
-        
+
         createUserCard(data)
         getRepos(username)
-    } catch (err) {
+    } catch(err) {
         if(err.response.status == 404) {
             createErrorCard('No profile with this username')
         }
@@ -19,15 +21,13 @@ async function getUser(username) {
 
 async function getRepos(username) {
     try {
-        const {data} = await axios(APIURL + username + '/repos?sort=created')
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
 
         addReposToCard(data)
-    } catch (err) {
+    } catch(err) {
         createErrorCard('Problem fetching repos')
-        
     }
 }
-
 
 function createUserCard(user) {
     const userID = user.name || user.login
@@ -35,30 +35,29 @@ function createUserCard(user) {
     const cardHTML = `
     <div class="card">
     <div>
-    <img src="${user.avatar_url}" alt="${user.name}" class="avatar_url">
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="user-info">
-    <h2>${userID}</h2>
-    ${userBio}
-    <ul>
-    <li>${user.followers} <strong>Followers</strong></li>
-    <li>${user.following} <strong>Following</strong></li>
-    <li>${user.public_repos} <strong>Repos</strong></li>
-    </ul>
-
-    <div id="repos"></div>
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+      <div id="repos"></div>
     </div>
-    </div>
+  </div>
     `
-    main.innerHTML= cardHTML
+    main.innerHTML = cardHTML
+    
 }
 
 function createErrorCard(msg) {
     const cardHTML = `
-    <div class="card">
-    <h1>${msg}</h1>
-    </div>
-    
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
     `
 
     main.innerHTML = cardHTML
@@ -67,23 +66,23 @@ function createErrorCard(msg) {
 function addReposToCard(repos) {
     const reposEl = document.getElementById('repos')
 
-    repos.slice(0, 5)
-    forEach(repo => {
-        const reposEl = document.createElement('a')
-        reposEl.classList.add('repo')
-        reposEl.href = repo.html_url
-        reposEl.target = '_blank'
-        reposEl.innerText = repo.name
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
 
-        reposEl.appendChild(reposEl)
-
-    });
+            reposEl.appendChild(repoEl)
+        })
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const user = search.ariaValueMax
+    const user = search.value
 
     if(user) {
         getUser(user)
@@ -91,4 +90,3 @@ form.addEventListener('submit', (e) => {
         search.value = ''
     }
 })
-
