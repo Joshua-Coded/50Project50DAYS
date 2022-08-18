3167a1f3097d3c64fea2070dc98dde7875a934b7
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-18 21:26:14 +0100

    script.js

--- live-user-filter/script.js
+++ live-user-filter/script.js
@@ -1,4 +1,4 @@
-const result = document.getElementById('result)
+const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
 
@@ -6,35 +6,37 @@ getData()
 
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
-async function getData(){
-const res = await fetch('https://randomuser.me/api?results=50')
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=50')
 
-				       const {result} = await res.json()
+    const { results } = await res.json()
 
-				       result.innerHTML = ''
+    // Clear result
+    result.innerHTML = ''
 
-				       results.forEach(user => {
-					   const li = document.createElement('li')
-					   listItems.push(li)
+    results.forEach(user => {
+        const li = document.createElement('li')
 
-					   li.innerHTML = `
-                                             <img src=""${user.picture.large}" alt="${user.name.first}" >
-<div class="user-info">
-<h4>${user.name.first} ${user.name.last}</h4>
-<p>${user.location.city}, ${user.location.country} </p>
-</div>
-`
+        listItems.push(li)
 
-					   result.appendChild(li)
-				       })
-				      }
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
 
-function filterData(searchItem) {
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
     listItems.forEach(item => {
-	if(item.innerText.toLowerCase().includes(searchItem.toLowerCase())) {
-	    item.classList.remove('hide')
-	} else {
-	    item.classList.add('hide')}
-
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
     })
 }
